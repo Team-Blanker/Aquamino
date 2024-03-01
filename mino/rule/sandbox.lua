7b2615cb496097cdad93980f8bc045bd0d199686
Team-Blanker <mintechoaqua@163.com>
2024-03-01 20:29:23 +0800

    所有块都可以在沙盒模式推箱子

--- mino/rule/sandbox.lua
+++ mino/rule/sandbox.lua
@@ -5,7 +5,7 @@ function ZNHJ.init(P,mino)
     mino.musInfo="Mikiya Komaba - Look Up The Starlight"
     mus.add('music/Hurt Record/Look Up The Starlight','parts','mp3',20.879,336*60/125)
     mus.start()
-    mino.rule.allowPush={O=true}
+    mino.rule.allowPush={Z=true,S=true,J=true,L=true,T=true,O=true,I=true,}
     mino.rule.loosen.fallTPL=.1
     mino.rule.allowSpin={Z=true,S=true,J=true,L=true,T=true,O=true,I=true,}
     for k,v in pairs(P) do
