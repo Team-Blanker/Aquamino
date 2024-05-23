ff849e7dffcf00793f1d4baa82d8492a6b352331
Team-Blanker <mintechoaqua@163.com>
2024-05-23 13:04:23 +0800

    调整T旋垃圾散乱度

--- mino/battle.lua
+++ mino/battle.lua
@@ -16,7 +16,7 @@ function battle.sendAtk(player,dest,atk)
 end
 function battle.atkRecv(player,atk)
     if atk.amount==0 then return end
-    print(atk.M_OC)
+    print(atk.M_IS)
     player.lastHole=rand()<atk.M_OC and rand(player.w) or player.lastHole
     local h
     for i=1,atk.amount do
@@ -36,7 +36,7 @@ function battle.stdAtkCalculate(player)
     local l,s,m,w,b,c=his.line,his.spin,his.mini,his.wide,his.B2B,his.combo
 
     local bl=(s and not m) and 2*l-1 or l>=4 and l or l-.5
-    local ba=b>0 and (9+b)/10 or 0
+    local ba=b>0 and (3+b)/4 or 0
     local ca=(w>=2 and w<=4) and min(c-1,.5) or min((c-1)/3+.01,3.34)
     local pc=his.PC and 6.67 or 0
     return l==0 and 0 or floor(bl+ba+ca+pc)
@@ -46,7 +46,7 @@ function battle.stdAtkGen(player)
     local l,s,m,w,b,c=his.line,his.spin,his.mini,his.wide,his.B2B,his.combo
 
     local bl=(s and not m) and 2*l-1 or l>=4 and l or l-.5
-    local ba=b>0 and (9+b)/10 or 0
+    local ba=b>0 and (3+b)/4 or 0
     local ca=(w>=2 and w<=4) and min(c-1,.5) or min((c-1)/3+.01,3.34)
     local pc=his.PC and 6.67 or 0
     local atk=floor(bl+ba+ca+pc)
@@ -54,7 +54,7 @@ function battle.stdAtkGen(player)
     return {
         amount=atk,
         block='g1',
-        M_IS=(w>=2 and w<=4 or his.PC) and 0 or s and (.5-.125*b)/(l*2-1) or 0,
+        M_IS=(w>=2 and w<=4 or his.PC) and 0 or s and (.2-.05*b) or 0,
         M_OC=(w>=2 and w<=4 or his.PC) and 0 or b>0 and 1/b or 1
     }
 end
