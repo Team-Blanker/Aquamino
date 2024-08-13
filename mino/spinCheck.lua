128f31c5511b779724e2e3b98a8a77d55b844ee0
Team-Blanker <mintechoaqua@163.com>
2024-08-13 20:05:19 +0800

    1

--- mino/spinCheck.lua
+++ mino/spinCheck.lua
@@ -4,7 +4,7 @@ local M,T=mymath,mytable
 --return spin,mini
 function spinCheck.default(player)
     local C=player.cur
-    if T.include({'T'},C.name) then
+    if C.name=='T' then
         if fLib.isImmobile(player) then return true,fLib.corner(player)<3
         else return false,false end
     else
