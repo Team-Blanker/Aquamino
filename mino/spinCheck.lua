274ee2944ea183c3d9d7a4f9c40cf2acc9e5aa09
Team-Blanker <mintechoaqua@163.com>
2024-08-13 20:04:57 +0800

    spin判定修改

--- mino/spinCheck.lua
+++ mino/spinCheck.lua
@@ -4,11 +4,12 @@ local M,T=mymath,mytable
 --return spin,mini
 function spinCheck.default(player)
     local C=player.cur
-    if T.include({'Z','S','O','I','J','L'},C.name) then
-        if fLib.isImmobile(player) then return true,false end
-    elseif T.include({'T'},C.name) then
+    if T.include({'T'},C.name) then
         if fLib.isImmobile(player) then return true,fLib.corner(player)<3
         else return false,false end
-    else return false,false end
+    else
+        if fLib.isImmobile(player) then return true,false
+        else return false,false end
+    end
 end
 return spinCheck
