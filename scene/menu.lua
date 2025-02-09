2724f930629fa44868e888e7940cf5ed8aff50d6
Team-Blanker <mintechoaqua@163.com>
2025-02-10 03:53:52 +0800

    1

--- scene/menu.lua
+++ scene/menu.lua
@@ -45,7 +45,7 @@ menu.secretMode={
     ['tower defense']={mode='core destruction',arg='tower defense'},
     sandbox={mode='square'},
     ['dig 40']={mode='dig bomb'},
-    battle={mode='bomb battle',arg='battle'}
+    --battle={mode='bomb battle',arg='battle'}
 }
 menu.notRecordScore={sandbox=true,battle=true,['tower defense']=true}
 menu.icon={}
