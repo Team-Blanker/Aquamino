2d0a3acba929f1ba69c4a8a0ee34c730be869478
Team-Blanker <mintechoaqua@163.com>
2025-03-18 02:16:23 +0800

    1

--- mino/spinCheck.lua
+++ mino/spinCheck.lua
@@ -12,4 +12,14 @@ function spinCheck.default(player)
         else return false,false end
     end
 end
+function spinCheck.AllMini(player)
+    local C=player.cur
+    if C.name=='T' then
+        if fLib.isImmobile(player) then return true,fLib.corner(player)<3
+        else return false,false end
+    else
+        if fLib.isImmobile(player) then return true,true
+        else return false,false end
+    end
+end
 return spinCheck
