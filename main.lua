ffb64e1c535d0fcdcdb67eaa58b70042610d7a91
Team-Blanker <mintechoaqua@163.com>
2025-05-15 00:31:00 +0800

    1

--- main.lua
+++ main.lua
@@ -130,7 +130,7 @@ end
 
 win={
     stat={launch=0,version="Beta V0.3.2.2",totalTime=0},
-    versionTxt="Beta V0.3.2.2",
+    versionTxt="Beta V0.3.2.2 Icosahedron",
     OS=love.system.getOS(),
     showInfo=false,
     fullscr=false,discardAfterDraw=false,
