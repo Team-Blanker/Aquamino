e18ffa135da4a34e3c0ac1b6b240887f20caf04f
Team-Blanker <mintechoaqua@163.com>
2025-05-30 23:45:00 +0800

    修正对战逻辑，消4应打4

--- mino/battle.lua
+++ mino/battle.lua
@@ -93,7 +93,7 @@ function battle.stdAtkCalculate(player)
 
     local bl=(s and not m) and 2*l-1 or l>=4 and 1.5*l-1.5 or l-.5 --基础攻击
     local ba=b>0 and min((3+b)/4,2.5) or 0 --B2B加成
-    local ca=max((c-3)/(2+2^(w-1))+(l>=4 and 1.5 or .5),0) --连消加成
+    local ca=max((c-3)/(2+2^(w-1))+((l>=4 and c>1) and 1.5 or .5),0) --连消加成
     return l==0 and 0 or floor(bl+ba+ca)
     end
 end
@@ -166,7 +166,7 @@ function battle.stdBombAtkCalculate(player)
 
     local bl=max(((s and not m) and l+min(l-1,3) or 0),(l>=4 and 1.5*l-1.5 or l-.5)) --基础攻击
     local ba=b>0 and min((3+b)/4,2.5) or 0 --B2B加成
-    local ca=max((c-3)/(2+2^(w-1))+(l>=4 and 1.5 or .5),0) --连消加成
+    local ca=max((c-3)/(2+2^(w-1))+((l>=4 and c>1) and 1.5 or .5),0) --连消加成
     return l==0 and 0 or floor(bl+ba+ca)
     end
 end
